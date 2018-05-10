bc3949afc6d36a1aa60cded297e1bf681a1bbf3a
Zhang Peng <zhang_p@worksap.co.jp>
2018-05-10 09:11:53 +0800

    Replace string with StandardCharset

--- src/main/java/com/github/cheergoivan/totp/decoder/Decoder.java
+++ src/main/java/com/github/cheergoivan/totp/decoder/Decoder.java
@@ -1,5 +1,10 @@
 package com.github.cheergoivan.totp.decoder;
 
 public interface Decoder {
+	/**
+	 * Return the byte representation of the given string
+	 * @param input
+	 * @return The byte representation of the given string
+	 */
 	byte[] decode(String input);
 }
--- src/main/java/com/github/cheergoivan/totp/decoder/SHA1Decoder.java
+++ src/main/java/com/github/cheergoivan/totp/decoder/SHA1Decoder.java
@@ -1,23 +1,21 @@
 package com.github.cheergoivan.totp.decoder;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class SHA1Decoder implements Decoder{
+public class SHA1Decoder implements Decoder {
+
+	private static final String ALGORITHM = "SHA-1";
 
 	@Override
 	public byte[] decode(String input) {
-		MessageDigest msgDigest = getMessageDigest("SHA-1");
-		try {
-			msgDigest.update(input.getBytes("UTF-8"), 0, input.length());
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalArgumentException(e);
-		}
+		MessageDigest msgDigest = getMessageDigest(ALGORITHM);
+		msgDigest.update(input.getBytes(StandardCharsets.UTF_8), 0, input.length());
 		return msgDigest.digest();
 	}
-	
-	private MessageDigest getMessageDigest(String algorithm){
+
+	private MessageDigest getMessageDigest(String algorithm) {
 		try {
 			return MessageDigest.getInstance(algorithm);
 		} catch (NoSuchAlgorithmException e) {
